59cd6470d4968afc9573fdda418d1995d8a354d8
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-04-16 14:00:43 -0400

    Toggle for GCP secrets engine

--- vault/terraform/main.tf
+++ vault/terraform/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = "~>0.12"
-  backend "remote" {
-    organization = "hashicorp-team-demo"
-    workspaces {
-      name = "nodejs-circleci-vault"
-    }
-  }
 }
 
 provider "google" {
---
+++ vault/terraform/toggles.tf
@@ -0,0 +1,5 @@
+variable "enable_gcp_secrets_engine" {
+  default     = false
+  description = "Toggle to enable GCP secrets engine"
+  type        = bool
+}
