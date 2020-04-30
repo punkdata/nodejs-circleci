eba5cfe107db867f8338eeb9d4c83f6b3e439a82
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-04-30 11:09:09 -0400

    Extend secret time

--- vault/terraform/approle.tf
+++ vault/terraform/approle.tf
@@ -20,7 +20,7 @@ resource "vault_approle_auth_backend_role" "app" {
   backend            = vault_auth_backend.approle.path
   role_name          = var.app
   token_policies     = [vault_policy.policy.name]
-  secret_id_ttl      = 3600
+  secret_id_ttl      = 259200
   secret_id_num_uses = 0
   token_num_uses     = 10
   token_ttl          = 3600
